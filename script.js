67a053afc84073536527830143018f1eb235ca70
faizashermuhammad <49517190+faizashermuhammad@users.noreply.github.com>
2019-07-25 17:56:59 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,117 @@
-let randomNumber;
-let wrongGuessCount=0;
-
-function resetGame() {
-    randomNumber = parseInt(Math.random() * 100);
-    wrongGuessCount = 0;
-}
-resetGame();
-
-function guessTheNumber() {
-    let userInput = parseInt(document.querySelector("input").value );
-
-    if (userInput < randomNumber) {
-        alert("To small");
-        wrongGuessCount++;
+let students = [
+    {
+        name: "Amna",
+        gender: "f",
+        dob: new Date("02-04-1990"),
+        address: {
+            ilaqa: "Gulistan-e-Johar",
+            city: "Karachi",
+            country: "Pakistan",
+            postalCode: 47114
+        },
+        phoneNo: "0331-2324243",
+        admissionTestScore: 56,
+        hasInternet: true,
+        hasComputer: false,
+        hasJob: true,
+        hasSchoolBefore: false
+    },
+    {
+        name: "Hadia",
+        gender: "f",
+        dob: new Date("05-15-1984"),
+        address: {
+            ilaqa: "Lyari",
+            city: "Karachi",
+            country: "Pakistan",
+            postalCode: 75660
+        },
+        phoneNo: "0345-3452953",
+        admissionTestScore: 48,
+        hasInternet: false,
+        hasComputer: false,
+        hasJob: false,
+        hasSchoolBefore: true
+    },
+    {
+        name: "Ahmed",
+        gender: "m",
+        dob: new Date("06-27-2002"),
+        address: {
+            ilaqa: "University Road",
+            city: "Quetta",
+            country: "Pakistan",
+            postalCode: 82215
+        },
+        phoneNo: "0333-0124325",
+        admissionTestScore: 33,
+        hasInternet: true,
+        hasComputer: false,
+        hasJob: false,
+        hasSchoolBefore: false
+    },
+    {
+        name: "Fariha",
+        gender: "f",
+        dob: new Date("09-13-1998"),
+        address: {
+            ilaqa: "University Road",
+            city: "Karachi",
+            country: "Pakistan",
+            postalCode: 82215
+        },
+        phoneNo: "0331-9432532",
+        admissionTestScore: 33,
+        hasInternet: true,
+        hasComputer: false,
+        hasJob: false,
+        hasSchoolBefore: false
+    },
+    {
+        name: "Abdullah",
+        gender: "m",
+        dob: new Date("01-24-1972"),
+        address: {
+            ilaqa: "Bazar Colony",
+            city: "Lahore",
+            country: "Pakistan",
+            postalCode: 32212
+        },
+        phoneNo: "0345-9912121",
+        admissionTestScore: 33,
+        hasInternet: false,
+        hasComputer: false,
+        hasJob: true,
+        hasSchoolBefore: true
     }
-    else if (userInput > randomNumber) {
-        alert("To Big");
-        wrongGuessCount++;
-    } else {
-        alert("congratulation! wrong guess " + wrongGuessCount);
-        resetGame();
-    }
+];
+// for (let i = 0; i < students.length; i++) {
+
+//     if (students[i].gender === "f") {
+//         console.log(students[i].name);
+//     }
+// }
+//Print names of male students only.
+//for (let i = 0 ; i < students.length ; i++) {
+    //if (students[i]. gender==="m"){
+      //  console.log(students[i].name);
+    //}
+//}
+//Print names of students who have passed the admission test. Passing marks are 50.
+// for(let i = 0 ; i < students.length ; i++){
+// if (students[i].admissionTestScore >=50){
+//     console.log(students[i].name);
+// }
+// }
+//Print names of eligible students only (students who have internet and live in Karachi are eligible)
+// for(let i = 0 ; i < students.length ; i++) {
+//     if(students[i].hasInternet students[i].karachi){
+//  console.log (students[i].name);
+// };
+// }
+ for(let i = 0 ; i < students.length ; i++) {
+if(students(i).hasJob=== true|| studend(i).hasSchoolBefore===true){
 }
+ }
+ console.log("group A": " +group.join(' , ;));
+ console.log("group B": " +group.join(' , ;));
